8228d63c6dd3f4a144fe55c8200c2367072ea4c4
jk <184119493@qq.com>
2016-05-15 12:02:52 +0800

    项目初始化 要加油了，尽量在一个月内完结。 这是一个练习项目。 起始时间2016年5月15日12:02:46

---
+++ gen/com/jk184/jksafe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jk184.jksafe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/jk184/jksafe/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.jk184.jksafe.activity;
+
+import com.jk184.jksafe.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
